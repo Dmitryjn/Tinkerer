feb738462f09d74708a2b3cd27a5874aaa2a9529
Agustin Navas <agustin@iMac-de-Agustin.local>
2021-02-09 18:39:09 -0500

    some shit

--- Tinkerer/Location.swift
+++ Tinkerer/Location.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct location: Decodable {
+struct location: Decodable, Identifiable {
     let id: Int
     let name: String
     let country: String
--- Tinkerer/WorldView.swift
+++ Tinkerer/WorldView.swift
@@ -22,8 +22,14 @@ struct WorldView: View {
     var body: some View {
         Map(coordinateRegion: $region , annotationItems: locations.places)
         {
-            
-            
+            location in
+            MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)){
+            Image(location.country)
+                .resizable()
+                .cornerRadius(10)
+                .frame(width:80, height:40)
+                .shadow(radius: 3)
+            }
         }
             .navigationTitle("Locations")
     }
